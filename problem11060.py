b23a8111f3d2e33a6cf92d706c6044317b5bdf1b
zjacom <eephus625@icloud.com>
2024-03-16 02:26:15 +0900

    PST
    Problem Solved Today

---
+++ problem11060.py
@@ -0,0 +1,22 @@
+import sys
+sys.setrecursionlimit(10 ** 6)
+
+N = int(input())
+
+arr = list(map(int, input().split()))
+
+dp = [sys.maxsize] * len(arr)
+
+def recur(idx):
+    if dp[idx] != sys.maxsize:
+        return dp[idx]
+    if idx == N - 1:
+        return 0
+    for nxt in range(1, arr[idx] + 1):
+        if idx + nxt < N:
+            dp[idx] = min(dp[idx], recur(idx + nxt) + 1)
+    
+    return dp[idx]
+
+recur(0)
+print(dp[0]) if dp[0] != sys.maxsize else print(-1)
