444b0155c4b7da73a4a854d1bf461ffdb03a7c78
zjacom <eephus625@icloud.com>
2024-03-27 10:37:54 +0900

    PRT
    Problem Reviewed Today

--- review.py
+++ review.py
@@ -1,15 +1,31 @@
-# 10422번
+# 11066번
+import sys
 
+def file_union(K, arr):
+    dp = [[sys.maxsize] * K for _ in range(K)]
+
+    for i in range(K):
+        dp[i][i] = 0
+
+    for x in range(1, K):
+        for y in range(x - 1, -1, -1):
+            if x - y == 1:
+                dp[y][x] = arr[y] + arr[x]
+            else:
+                cost = sys.maxsize
+                for nx in range(x):
+                    cost = min(cost, dp[y][nx] + dp[nx + 1][x])
+                dp[y][x] = cost + sum(arr[y:x + 1])
+    return dp[0][-1]
+
+answer = []
 T = int(input())
-answer = [int(input()) for _ in range(T)]
+for _ in range(T):
+    k = int(input())
+    a = list(map(int, input().split()))
 
-dp = [0] * 5001
-dp[0] = 1
+    answer.append(file_union(k, a))
 
-for idx in range(2, 5001, 2):
-    for jdx in range(2, idx + 1, 2):
-        dp[idx] += dp[idx - jdx] * dp[jdx - 2]
-    dp[idx] = dp[idx] % 1_000_000_007
 
 for ans in answer:
-    print(dp[ans])
+    print(ans)
