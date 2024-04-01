7f6744fe646239016ce674c4aa3d66a632fffb41
zjacom <eephus625@icloud.com>
2024-04-01 11:11:11 +0900

    PRT
    Problem Reviewed Today

--- problem9376.py
+++ problem9376.py
@@ -1,59 +1,59 @@
-import sys
 from collections import deque
+import sys
 
-answer = []
+TC = int(input())
+result = []
 dy, dx = [0, 0, 1, -1], [1, -1, 0, 0]
-T = int(input())
 
-def bfs(y, x):
-    visited = [[0] * (w + 2) for _ in range(h + 2)]
+def bfs(cy, cx):
+    visited = [[-1] * (w + 2) for _ in range(h + 2)]
+    visited[cy][cx] = 0
 
-    q = deque([(y, x, 0)])
-
+    q = deque([(cy, cx)])
     while q:
-        cy, cx, cnt = q.popleft()
+        y, x = q.popleft()
         for i in range(4):
-            ny, nx = cy + dy[i], cx + dx[i]
+            ny, nx = y + dy[i], x + dx[i]
             if 0 <= ny < h + 2 and 0 <= nx < w + 2:
-                if not visited[ny][nx] and graph[ny][nx] != '*':
-                    visited[ny][nx] = 1
-                    if dist[ny][nx] < 0:
-                        dist[ny][nx] = 0
+                if visited[ny][nx] == -1:
                     if graph[ny][nx] == "#":
-                        q.append((ny, nx, cnt + 1))
-                        dist[ny][nx] += cnt + 1
-                    else:
-                        q.appendleft((ny, nx, cnt))
-                        dist[ny][nx] += cnt
+                        visited[ny][nx] = visited[y][x] + 1
+                        q.append((ny, nx))
+                    elif graph[ny][nx] == "." or graph[ny][nx] == "$":
+                        visited[ny][nx] = visited[y][x]
+                        q.appendleft((ny, nx))
+    return visited
 
 
-for _ in range(T):
+for _ in range(TC):
     h, w = map(int, input().split())
-    graph = [['.'] * (w + 1)]
+    graph = [["."] * (w + 2)]
     for _ in range(h):
-        graph.append(['.'] + list(input()) + ['.'])
-    graph.append(['.'] * (w + 1))
+        graph.append(["."] + list(input().strip()) + ["."])
+    graph.append(["."] * (w + 2))
 
-    dist = [[-1] * (w + 2) for _ in range(h + 2)]
-
-    prisoners = []
-    for i in range(h):
-        for j in range(w):
-            if graph[i+1][j+1] == "$":
-                prisoners.append((i+1, j+1))
-                graph[i+1][j+1] = '.'
-
-    bfs(prisoners[0][0], prisoners[0][1])
-    bfs(prisoners[1][0], prisoners[1][1])
-    bfs(0, 0)
+    points = []
+    for y in range(h + 2):
+        for x in range(w + 2):
+            if graph[y][x] == "$":
+                points.append((y, x))
+    
+    c1 = bfs(points[0][0], points[0][1])
+    c2 = bfs(points[1][0], points[1][1])
+    sang = bfs(0, 0)
 
     answer = sys.maxsize
+
     for y in range(1, h + 1):
         for x in range(1, w + 1):
-            if dist[y][x] == -1:
-                continue
-            if graph[y][x] == "#":
-                answer = min(answer, dist[y][x] - 2)
-            elif graph[y][x] == '.':
-                answer = min(answer, graph[y][x])
-    print(answer)
+            if c1[y][x] != -1 and c2[y][x] != -1 and sang[y][x] != -1:
+                cnt = c1[y][x] + c2[y][x] + sang[y][x]
+                if graph[y][x] == "#":
+                    answer = min(answer, cnt - 2)
+                else:
+                    answer = min(answer, cnt)
+
+    result.append(answer)
+
+for res in result:
+    print(res)
--- review.py
+++ review.py
@@ -1,10 +1,50 @@
-# 11058번
+# 6087번
+from collections import deque
+import sys
 
-N = int(input())
+W, H = map(int, input().split())
 
-dp = [num for num in range(100 + 1)]
+graph = [list(input().strip()) for _ in range(H)]
+points = []
+visited = [[[sys.maxsize] * 4 for _ in range(W)] for _ in range(H)]
+dy, dx = [0, 0, 1, -1], [1, -1, 0, 0]
 
-for idx in range(7, 100 + 1):
-    dp[idx] = max(dp[idx - 5] * 4, dp[idx - 4] * 3, dp[idx - 3] * 2)
+for y in range(H):
+    for x in range(W):
+        if graph[y][x] == "C":
+            points.append((y, x))
 
-print(dp[N])
+q = deque()
+for i in range(4):
+    ny, nx = points[0][0] + dy[i], points[0][1] + dx[i]
+    if 0 <= ny < H and  0 <= nx < W:
+        if graph[ny][nx] != "*":
+            visited[ny][nx][i] = 0
+            q.append((ny, nx, i))
+
+while q:
+    y, x, d = q.popleft()
+    for i in range(4):
+        ny, nx = y + dy[i], x + dx[i]
+        if 0 <= ny < H and  0 <= nx < W:
+            if graph[ny][nx] != "*":
+                if i == 0 or i == 1:
+                    if d == 2 or d == 3:
+                        if visited[y][x][d] + 1 < visited[ny][nx][i]:
+                            q.append((ny, nx, i))
+                            visited[ny][nx][i] = visited[y][x][d] + 1
+                    else:
+                        if visited[y][x][d] < visited[ny][nx][i]:
+                            q.append((ny, nx, i))
+                            visited[ny][nx][i] = visited[y][x][d]
+                if i == 2 or i == 3:
+                    if d == 0 or d == 1:
+                        if visited[y][x][d] + 1 < visited[ny][nx][i]:
+                            q.append((ny, nx, i))
+                            visited[ny][nx][i] = visited[y][x][d] + 1
+                    else:
+                        if visited[y][x][d] < visited[ny][nx][i]:
+                            q.append((ny, nx, i))
+                            visited[ny][nx][i] = visited[y][x][d]
+
+print(min(visited[points[1][0]][points[1][1]]))
