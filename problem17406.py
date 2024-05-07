1ab77c8e037aa4f2b4241b772eb57425e19e9148
zjacom <eephus625@icloud.com>
2024-05-07 12:05:58 +0900

    PST
    Problem Solved Today

--- problem17406.py
+++ problem17406.py
@@ -2,85 +2,38 @@ import sys
 from itertools import permutations
 
 N, M, K = map(int, input().split())
-
-AAA = [list(map(int, input().split())) for _ in range(N)]
+graph = [list(map(int, input().split())) for _ in range(N)]
 spins = []
-
 for _ in range(K):
-    a, b, c = map(int, input().split())
-    spins.append((a, b, c))
+    r, c, s = map(int, input().split())
+    spins.append((r, c, s))
 
+spins = list(permutations(spins, K))
+result = sys.maxsize
 
-def func(p1, p2, graph):
-    lu, ld, ru, rd = (p1[0], p1[1]), (p2[0], p1[1]), (p1[0], p2[1]), (p2[0], p2[1])
+def func(spin, graph):
+    global result
+    temp = [[n for n in graph[i]] for i in range(len(graph))]
 
-    arr = [[n for n in graph[i]] for i in range(N)]
+    for r, c, s in spin:
+        for ss in range(s, 0, -1):
+            lu, rd = (r - ss - 1, c - ss - 1), (r + ss - 1, c + ss - 1)
+            for x in range(lu[1] + 1, rd[1] + 1):
+                graph[lu[0]][x] = temp[lu[0]][x - 1]
+            for y in range(lu[0] + 1, rd[0] + 1):
+                graph[y][rd[1]] = temp[y - 1][rd[1]]
+            for x in range(rd[1] - 1, lu[1] - 1, -1):
+                graph[rd[0]][x] = temp[rd[0]][x + 1]
+            for y in range(rd[0] - 1, lu[0] - 1, -1):
+                graph[y][lu[1]] = temp[y + 1][lu[1]]
+            
+        temp = [[n for n in graph[i]] for i in range(len(graph))]
 
-    HHH, A, B = lu[0], lu[1] + 1, ru[1]
-    while B - A > 0:
-        for x in range(A, B):
-            arr[HHH][x] = graph[HHH][x - 1]
-        A += 1
-        B -= 1
-        HHH += 1
-    
-    HH, C, D = ld[0], ld[1] + 1, rd[1]
-    while D - C > 0:
-        for x in range(C, D):
-            arr[HH][x] = graph[HH][x + 1]
-        C += 1
-        D -= 1
-        HH -= 1
-    
-    WW, E, F = lu[1], lu[0] + 1, ld[0]
-    while F - E > 0:
-        for y in range(E, F):
-            arr[y][WW] = graph[y + 1][WW]
-        E += 1
-        F -= 1
-        WW += 1
-    
-    WWW, G, H = ru[1], ru[0] + 1, rd[0]
-    while H - G > 0:
-        for y in range(G, H):
-            arr[y][WWW] = graph[y - 1][WWW]
-        G += 1
-        H -= 1
-        WWW -= 1
-    
-    while lu != ld != ru != rd:
-        arr[lu[0]][lu[1]] = graph[lu[0] + 1][lu[1]]
-        arr[ru[0]][ru[1]] = graph[ru[0]][ru[1] - 1]
-        arr[ld[0]][ld[1]] = graph[ld[0]][ld[1] + 1]
-        arr[rd[0]][rd[1]] = graph[rd[0] - 1][rd[1]]
+    for row in graph:
+        result = min(result, sum(row))
+        
+for spin in spins:
+    temp = [[n for n in graph[i]] for i in range(len(graph))]
+    func(spin, temp)
 
-        lu = (lu[0] + 1, lu[1] + 1)
-        ru = (ru[0] + 1, ru[1] - 1)
-        ld = (ld[0] - 1, ld[1] + 1)
-        rd = (rd[0] - 1, rd[1] - 1)
-    
-    return arr
-
-
-def calculator(arr):
-    total = sys.maxsize
-    for y in range(N):
-        total = min(total, sum(arr[y]))
-    
-    return total
-
-
-if K == 1:
-    result = func((spins[0][0] - spins[0][2] - 1, spins[0][1] - spins[0][2] - 1), (spins[0][0] + spins[0][2] - 1, spins[0][1] + spins[0][2] - 1), AAA)
-    answer = calculator(result)
-    print(answer)
-else:
-    answer = sys.maxsize
-    for order in list(permutations(spins, K)):
-        result = [[n for n in AAA[i]] for i in range(N)]
-        for i in range(len(order)):
-            p1, p2 = (order[i][0] - order[i][2] - 1, order[i][1] - order[i][2] - 1), (order[i][0] + order[i][2] - 1, order[i][1] + order[i][2] - 1)
-            result = func(p1, p2, result)
-        answer = min(answer, calculator(result))
-
-print(answer)
+print(result)
