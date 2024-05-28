35d1bec0a79472d532b98d17edbee91519b166c7
zjacom <eephus625@icloud.com>
2024-05-28 18:37:04 +0900

    PRT
    Problem Reviewed Today

--- review.py
+++ review.py
@@ -1,12 +1,52 @@
-# 2056번
+# 14263번
+from collections import defaultdict
+import heapq
 
-N = int(input())
-dp = [0] * (N + 1)
+N, M = map(int, input().split())
 
-for i in range(1, N + 1):
-    cost, count, *prev = map(int, input().split())
-    dp[i] = cost
-    for j in prev:
-        dp[i] = max(dp[i], dp[j] + cost)
-    
-print(max(dp))
+grid = [list(input()) for _ in range(N)]
+
+dic = defaultdict(lambda: [M, 0, 0, N]) # left, right, down, up
+
+for y in range(N):
+    for x in range(M):
+        if grid[y][x] != ".":
+            card = grid[y][x]
+            dic[card][0] = min(dic[card][0], x)
+            dic[card][1] = max(dic[card][1], x)
+            dic[card][2] = max(dic[card][2], y)
+            dic[card][3] = min(dic[card][3], y)
+
+indegrees = defaultdict(int)
+graph = defaultdict(list)
+
+for card, (left, right, down, up) in dic.items():
+    for y in range(up, down + 1):
+        for x in range(left, right + 1):
+            if grid[y][x] == ".":
+                print(-1)
+                exit(0)
+            if grid[y][x] != card and grid[y][x] not in graph[card]:
+                indegrees[grid[y][x]] += 1
+                graph[card].append(grid[y][x])
+
+q = []
+
+for card in dic.keys():
+    if card not in indegrees:
+        heapq.heappush(q, card)
+
+order = []
+while q:
+    node = heapq.heappop(q)
+    order.append(node)
+
+    for nxt in graph[node]:
+        indegrees[nxt] -= 1
+        if indegrees[nxt] == 0:
+            heapq.heappush(q, nxt)
+
+if len(order) != len(dic):
+    print(-1)
+else:
+    print(''.join(order))
