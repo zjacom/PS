79ed418003c4cc05c1c7d5ef2b4e893ca6f82b3a
김승훈 <eephus625@icloud.com>
2024-10-25 10:54:39 +0900

    PST

---
+++ Leetcode/Monthly_Transactions_I.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select date_format(trans_date, '%Y-%m') month, country,
+count(*) trans_count,
+sum(case when state = "approved" then 1 else 0 end) approved_count,
+sum(amount) trans_total_amount,
+sum(case when state = "approved" then amount else 0 end) approved_total_amount
+from transactions
+group by date_format(trans_date, '%Y-%m'), country
